58ccaabdf136f702e8f85222d7fcc65d004af075
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-02-11 14:25:15 +0530

    Time: 2 ms (88.86%) | Memory: 49.4 MB (53.14%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -8,6 +8,56 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public void reorderList(ListNode head) {
+//         if(head==null||head.next==null)
+//         {
+//             return;
+//         }
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         while(fast!=null && fast.next!=null)
+//         {
+//           slow=slow.next;
+//           fast=fast.next.next;  
+//         }
+//         ListNode first = slow.next;
+//         slow.next = null;
+//         first = reverseList(first);
+//         mergeLists(head, first);
+
+//     }
+//       ListNode reverseList(ListNode head) {
+//         ListNode prev = null;
+//         ListNode temp = head;
+//         ListNode next;
+
+//         while (temp != null) {
+//             next = temp.next;
+//             temp.next = prev;
+//             prev = temp;
+//             temp = next;
+//         }
+//         return prev;
+//     }    
+//     public void mergeLists(ListNode head1, ListNode head2) {
+//         while (head1 != null && head2 != null) {
+//             ListNode temp1 = head1.next;
+//             ListNode temp2 = head2.next;
+
+//             head1.next = head2;
+//             if (temp1 == null) {
+//             break;
+//             }
+//             head2.next = temp1;
+
+//             head1 = temp1;
+//             head2 = temp2;
+//         }
+//     }
+// }
+
+
 class Solution {
     public void reorderList(ListNode head) {
         if(head==null||head.next==null)
@@ -16,45 +66,33 @@ class Solution {
         }
         ListNode slow=head;
         ListNode fast=head;
-        while(fast!=null && fast.next!=null)
+        while(fast.next!=null&&fast.next.next!=null)
         {
-          slow=slow.next;
-          fast=fast.next.next;  
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        ListNode first = slow.next;
-        slow.next = null;
-        first = reverseList(first);
-        mergeLists(head, first);
+        ListNode temp=slow.next;
+        ListNode next=null;
+        ListNode prev=null;
+        while(temp!=null)
+        {
+            next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
 
-    }
-      ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode temp = head;
-        ListNode next;
-
-        while (temp != null) {
-            next = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = next;
         }
-        return prev;
-    }    
-    public void mergeLists(ListNode head1, ListNode head2) {
-        while (head1 != null && head2 != null) {
-            ListNode temp1 = head1.next;
-            ListNode temp2 = head2.next;
-
-            head1.next = head2;
-            if (temp1 == null) {
-            break;
-            }
-            head2.next = temp1;
-
-            head1 = temp1;
-            head2 = temp2;
+        slow.next=null;
+        ListNode temp1=head;
+        ListNode temp2=prev;
+        while(temp2!=null)
+        {
+            ListNode next1=temp1.next;
+            ListNode next2=temp2.next;
+            temp1.next=temp2;
+            temp2.next=next1;
+            temp1=next1;
+            temp2=next2;
         }
     }
 }
-
-
