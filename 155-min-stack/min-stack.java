1bb7034c010bd67905da23e3a5677490bf29b64d
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-02-19 12:13:23 +0530

    Time: 4 ms (99.66%) | Memory: 47.1 MB (47.58%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,34 +1,78 @@
 class MinStack {
-    Stack<Integer>stack;
-    Stack<Integer>minstack;
-    
-
+    // Stack<Integer>stack;
+    // Stack<Integer>minstack;
+    Node head;
+    Node head1;
+    class Node{
+        int data;
+        Node next;
+    Node(int val)
+    {
+    data=val;
+    next=null;
+    }
+    }
     public MinStack() {
-        stack=new Stack<>();
-        minstack=new Stack<>();
-
-        
+        // stack=new Stack<>();
+        // minstack=new Stack<>();
     }
     
     public void push(int val) {
-        stack.push(val);
-        if(minstack.isEmpty()||minstack.peek()>=val)
+        Node newNode1=new Node(val);
+        Node newNode2=new Node(val);
+        if(head==null)
         {
-            minstack.push(val);
+            head=newNode1;
+        }
+        else
+        {
+            newNode1.next=head;
+            head=newNode1;
+        }
+        if(head1==null)
+        {
+            head1=newNode2;
+        }
+        else
+        {
+            if(head1.data>=newNode2.data)
+            {
+            newNode2.next=head1;
+            head1=newNode2;
+        }
         }
     }
+    //     stack.push(val);
+    //     if(minstack.isEmpty()||minstack.peek()>=val)
+    //     {
+    //         minstack.push(val);
+    //     }
+    // }
     public void pop() {
-        if(stack.peek().equals(minstack.peek()))
-        {
-            minstack.pop();
-        }
-        stack.pop();
+    //     if(stack.peek().equals(minstack.peek()))
+    //     {
+    //         minstack.pop();
+    //     }
+    //     stack.pop();
+    // }
+    if(head==null)
+    {
+        return;
     }
+    if(head.data==head1.data)
+    {
+        head1=head1.next;
+    }
+    head=head.next;
+    }
+    
     public int top() {
-        return stack.peek();
+        return head.data;
+        //return stack.peek();
     }
     public int getMin() {
-        return minstack.peek();
+        return head1.data;
+       // return minstack.peek();
 
     }
 }
