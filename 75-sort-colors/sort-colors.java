6c0dafdffb231969630eaac68d157669589ae3a0
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-03-04 15:11:06 +0530

    Time: 2 ms (19.17%) | Memory: 43.6 MB (60.44%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        //Arrays.sort(nums);
+        // Selection Sort approach - O(n²) time, O(1) space
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIndex = i;  // Track index of minimum element
+            
+            // Find minimum element in remaining unsorted array
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            // Swap nums[i] and nums[minIndex]
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        
+        // void function - no return statement needed!
     }
 }
