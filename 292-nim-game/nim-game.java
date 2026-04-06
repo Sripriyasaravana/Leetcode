db5fe017ad2d7c28cfd89f0418953e75891ab05b
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-04-06 19:25:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (63.13%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n%4!=0) return true;
-        else return false;
-        
-        
+        // if(n%4!=0) return true;
+        // else return false;
+
+        return(n%4!=0);
     }
 }
