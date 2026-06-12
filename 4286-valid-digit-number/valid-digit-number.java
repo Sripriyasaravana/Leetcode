18782f16d293d968bc31a18c33aae03714f98551
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-12 13:15:33 +0530

    Time: 1 ms (99.03%) | Memory: 42.7 MB (37.22%) - LeetSync

--- 4286-valid-digit-number/valid-digit-number.java
+++ 4286-valid-digit-number/valid-digit-number.java
@@ -1,19 +1,32 @@
+// class Solution {
+//     public boolean validDigit(int n, int x) {
+//         String number = Integer.toString(n);
+
+//         // Check if the first digit is x
+//         if (number.charAt(0) - '0' == x) {
+//             return false;
+//         }
+
+//         // Check if x is present in the number
+//         for (int i = 0; i < number.length(); i++) {
+//             if (number.charAt(i) - '0' == x) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean validDigit(int n, int x) {
-        String number = Integer.toString(n);
-
-        // Check if the first digit is x
-        if (number.charAt(0) - '0' == x) {
-            return false;
-        }
-
-        // Check if x is present in the number
-        for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) - '0' == x) {
-                return true;
+        boolean occurrenceX = false;
+        while (n >= 10) {
+            if (n % 10 == x) {
+                occurrenceX = true;
             }
+            n /= 10;
         }
-
-        return false;
+        return occurrenceX && n != x;
     }
 }
