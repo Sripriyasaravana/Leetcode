7961114e04ac4c5a09b9a999e996c6b52e76708a
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-16 20:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (27.53%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-       // int n=sc.nextInt();
-        int rev1=0;
-        int temp=num;
-        while(temp>0)
-        {
-            int digits=temp%10;
-            rev1=rev1*10+digits;
-            temp=temp/10;
-        }
-        int rev2=0;
-        temp=rev1;
-        while(temp>0)
-        {
+//        // int n=sc.nextInt();
+//         int rev1=0;
+//         int temp=num;
+//         while(temp>0)
+//         {
+//             int digits=temp%10;
+//             rev1=rev1*10+digits;
+//             temp=temp/10;
+//         }
+//         int rev2=0;
+//         temp=rev1;
+//         while(temp>0)
+//         {
             
-            int digits=temp%10;
-            rev2=rev2*10+digits;
-            temp=temp/10;
+//             int digits=temp%10;
+//             rev2=rev2*10+digits;
+//             temp=temp/10;
         
-        }
-        if(rev2==num)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+//         }
+//         if(rev2==num)
+//         {
+//             return true;
+//         }
+//         else
+//         {
+//             return false;
+//         }
         
+//     }
+// }
+return num==0||num%10!=0;
     }
 }
