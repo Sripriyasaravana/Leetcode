ec906a4fa4d931a929b209ecbf1faaa568a0c70f
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-17 15:11:22 +0530

    Time: 5 ms (6.67%) | Memory: 46.6 MB (5.28%) - LeetSync

--- 4229-trim-trailing-vowels/trim-trailing-vowels.java
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.java
@@ -1,11 +1,36 @@
 class Solution {
     public String trimTrailingVowels(String s) {
-        int n = s.length() - 1;
+//         int n = s.length() - 1;
 
-        while (n>=0 && "aeiou".contains(s.substring(n,n+1))) {
-            n--;
+//         while (n>=0 && "aeiou".contains(s.substring(n,n+1))) {
+//             n--;
+//         }
+
+//         return s.substring(0, n + 1);
+//     }
+// }
+
+
+       
+
+        int i = s.length() - 1;
+
+        while (i >= 0 &&
+              (s.charAt(i) == 'a' ||
+               s.charAt(i) == 'e' ||
+               s.charAt(i) == 'i' ||
+               s.charAt(i) == 'o' ||
+               s.charAt(i) == 'u')) {
+
+            i--;
         }
 
-        return s.substring(0, n + 1);
+        String output = "";
+
+        for (int j = 0; j <= i; j++) {
+            output += s.charAt(j);
+        }
+
+        return output;
     }
 }
