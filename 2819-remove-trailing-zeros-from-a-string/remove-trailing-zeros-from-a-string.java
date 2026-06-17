488cdf640a28e8243c1f9e244d428c5c2ea4405d
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-17 14:38:25 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (95.12%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -1,14 +1,14 @@
-// class Solution {
-//     public String removeTrailingZeros(String num) {
-//         int n=num.length()-1;
-//             while(n>0&&num.charAt(n)==0)
-//             {
-//                 i--;
-//             }
-//             return num.substring(0,n+1);
-//         }    
-        
-//     }
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int n = num.length() - 1;
+
+        while (n > 0 && num.charAt(n) == '0') {
+            n--;
+        }
+
+        return num.substring(0, n + 1);
+    }
+}
     // int i=s.length()-1;
     // while(s[i]==0)
     // {
@@ -21,21 +21,21 @@
 
     // }
     
-    class Solution {
-    public String removeTrailingZeros(String num) {
+//     class Solution {
+//     public String removeTrailingZeros(String num) {
 
-        int i = num.length() - 1;
-        while (i >= 0 && num.charAt(i) == '0') {
-            i--;
-        }
-        // if (i == -1) {
-        //     return "0";
-        // }
-        String output = "";
-        for (int j = 0; j <= i; j++) {
-            output += num.charAt(j);
-        }
+//         int i = num.length() - 1;
+//         while (i >= 0 && num.charAt(i) == '0') {
+//             i--;
+//         }
+//         // if (i == -1) {
+//         //     return "0";
+//         // }
+//         String output = "";
+//         for (int j = 0; j <= i; j++) {
+//             output += num.charAt(j);
+//         }
 
-        return output;
-    }
-}
+//         return output;
+//     }
+// }
