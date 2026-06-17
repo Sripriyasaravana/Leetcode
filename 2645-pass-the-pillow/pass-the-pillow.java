4c50c0b41a84853137349d77203b807abc0cca4b
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-17 11:24:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.95%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -1,33 +1,33 @@
 class Solution {
     public int passThePillow(int n, int time) {
-//         int current=time/(n-1);
-//         int direction=time%(n-1);
-//         int ans=0;
-//         if(current%2!=0)
-//         {
-//             ans=n-direction;
-//         }
-//         else
-//         {
-//             ans=n+direction;
-//         }
-//         return ans; 
+        int current=time/(n-1);
+        int direction=time%(n-1);
+        int ans=0;
+        if(current%2!=0)
+        {
+            ans=n-direction;
+        }
+        else
+        {
+            ans=1+direction;
+        }
+        return ans; 
+    }
+}
+// int current=1;
+// int direction=1;
+// for(int i=1;i<=time;i++)
+// {
+//     if(current==n)
+//     {
+//         direction=-1;
+//     }
+//     else if(current==1)
+//     {
+//         direction=1;
+//     }
+//     current+=direction;
+// }
+// return current;
 //     }
 // }
-int current=1;
-int direction=1;
-for(int i=1;i<=time;i++)
-{
-    if(current==n)
-    {
-        direction=-1;
-    }
-    else if(current==1)
-    {
-        direction=1;
-    }
-    current+=direction;
-}
-return current;
-    }
-}
