5dc5d7238b1511e738893133ace80a0c063746db
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-25 19:45:44 +0530

    Time: 1 ms (99.14%) | Memory: 42.8 MB (14.46%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -7,3 +7,11 @@ class Solution {
        return n==1;
     }
 }
+// if (n <= 0) return false;
+//         int[] factors = {2, 3, 5};
+//         for (int factor : factors) {
+//             while (n % factor == 0) {
+//                 n /= factor;
+//             }
+//         }
+//         return n == 1;  
