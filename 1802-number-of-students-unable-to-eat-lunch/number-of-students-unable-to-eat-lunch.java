61f5fec0a2c39c498991ed36684398fc4d208337
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-25 12:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (92.04%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int count0=0;
+        int count1=0;
+        for(int i=0;i<students.length;i++)
+        {
+            if(students[i]==0)
+            {
+                count0++;
+            }
+            else
+            {
+                count1++;
+            }
+        }
+        for(int i=0;i<sandwiches.length;i++)
+        {
+            if(sandwiches[i]==0)
+            {
+                if(count0==0)
+                {
+                    return count1;
+                }
+                count0--;
+            }
+            else
+            {
+                if(count1==0)
+                {
+                    return count0;
+                }
+                count1--;
+            }
+        }
+        return 0;
+    }
+}
