ba82ba3e823982be052d42fca211c2cad29c2cd8
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-06-26 09:49:29 +0530

    Time: 1 ms (99.57%) | Memory: 43.3 MB (98.71%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(String[] logs) {
+
+        int count = 0;
+
+        for (String s : logs) {
+
+            if (s.equals("../")) {
+                if (count > 0) {
+                    count--;
+                }
+            }
+            else if (s.equals("./")) {
+                continue;
+            }
+            else {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
