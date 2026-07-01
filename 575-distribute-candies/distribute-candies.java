d67b7fa30601c63b0703c950bb22f157ab8aa043
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-07-01 11:24:48 +0530

    Time: 30 ms (93.23%) | Memory: 48.6 MB (49.89%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,23 +1,34 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        Arrays.sort(candyType);
-        int count=1;
-        for(int i=1;i<candyType.length;i++)
-        {
-            if(candyType[i]!=candyType[i-1])
-            {
-                count++;
-            }
+        // Arrays.sort(candyType);
+        // int count=1;
+        // for(int i=1;i<candyType.length;i++)
+        // {
+        //     if(candyType[i]!=candyType[i-1])
+        //     {
+        //         count++;
+        //     }
+        // }
+        // int n=candyType.length/2;
+        // if(count<n){
+        //     return count;
+        // }
+        // else{
+        //     return n;
+        // }
+        int n = candyType.length;
+
+        HashSet<Integer> unique = new HashSet<>();
+
+        for(int candy : candyType){
+            unique.add(candy);
         }
-        int n=candyType.length/2;
-        if(count<n){
-            return count;
+
+        if(unique.size() >= n / 2){
+            return n / 2;
         }
         else{
-            return n;
+            return unique.size();
         }
-       
-
     }
 }
-
