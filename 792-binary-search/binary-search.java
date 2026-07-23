b1746b73f2dc8ad01101e64db10cd0d89790a4d5
Sripriyasaravana <sripriyasaravanan849@gmail.com>
2026-07-23 10:01:26 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (31.25%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,11 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==target)
-            {
-                return i;
+         int n=nums.length;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==target)
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int[]arr=nums;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return -1;
